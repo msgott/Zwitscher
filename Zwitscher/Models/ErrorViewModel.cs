d1c2a9918af1c19cd2bae9035b0ce74c5d15a271
Maik Gottfried <maikgottfried@live.de>
2023-04-27 15:31:19 +0200

    Projektdateien hinzufügen.

---
+++ Zwitscher/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zwitscher.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
