482e19342500a23caefda017d1f9331f72f3e90b
Pascal <pascal.reuter@student.uni-siegen.de>
2023-05-25 10:32:07 +0200

    added new file for test purposes, isolated from master branch on frontend1 branch

---
+++ Zwitscher/abc/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zwitscher.abc
+{
+    public class Class
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
