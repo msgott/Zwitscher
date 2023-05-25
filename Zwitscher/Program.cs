25d46941693020226fbd5e80b084fb1d804c15f4
Pascal <pascal.reuter@student.uni-siegen.de>
2023-05-25 15:22:00 +0200

    added clientapp folder with react

--- Zwitscher/Program.cs
+++ Zwitscher/Program.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Zwitscher.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ZwitscherContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings:ZwitscherContext") ?? throw new InvalidOperationException("Connection string 'ZwitscherContext' not found.")));
@@ -13,6 +17,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Zwitscher API", Version = "v1" });
+});
+
+// Necesarry for React SpaStatic Services
+builder.Services.AddSpaStaticFiles(configuration => {
+    configuration.RootPath = "clientapp/build";
 });
 
 
@@ -42,6 +51,47 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+// React
+var spaPath = "/Zwitscher";
+if (app.Environment.IsDevelopment())
+{
+    app.MapWhen(y => y.Request.Path.StartsWithSegments(spaPath), client =>
+    {
+        client.UseSpa(spa =>
+        {
+            spa.UseProxyToSpaDevelopmentServer("https://localhost:4444");
+        });
+    });
+}
+else
+{
+    app.Map(new PathString(spaPath), client =>
+    {
+        client.UseSpaStaticFiles();
+        client.UseSpa(spa => {
+            spa.Options.SourcePath = "clientapp";
+
+            // adds no-store header to index page to prevent deployment issues (prevent linking to old .js files)
+            // .js and other static resources are still cached by the browser
+            spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    ResponseHeaders headers = ctx.Context.Response.GetTypedHeaders();
+                    headers.CacheControl = new Microsoft.Net.Http.Headers.CacheControlHeaderValue
+                    {
+                        NoCache = true,
+                        NoStore = true,
+                        MustRevalidate = true
+                    };
+                }
+            };
+        });
+    });
+}
 
 app.Run();
 
